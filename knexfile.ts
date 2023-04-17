61df823ec06162b5c6e2097e5e6c68c00d1dcf3f
Restu Dwi Cahyo <dwi.restu3@gmail.com>
2023-04-17 14:33:21 +0700

    Add env for knex config

--- knexfile.ts
+++ knexfile.ts
@@ -1,13 +1,16 @@
 import { Knex } from "knex";
 import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const knexConfig: Knex.Config = {
   client: "postgresql",
   connection: {
-    host: "localhost",
-    user: "postgres",
-    password: "restuganteng",
-    database: "sample"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
   },
   pool: {
     min: 2,
