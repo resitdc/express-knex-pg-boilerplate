b240f2e538b4e2bc27c006821df7f2e3009a887f
Restu Dwi Cahyo <dwi.restu3@gmail.com>
2023-04-17 14:37:10 +0700

    Add absolute import

--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
 import express, { Application, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 3000;
