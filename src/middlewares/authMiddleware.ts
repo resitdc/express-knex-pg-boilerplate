03d173cf7011cbd6fa081ebd5c4ccbca077914aa
Restu Dwi Cahyo <restu@resitdc.id>
2023-05-05 01:25:21 +0700

    Auth middleware

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { errorResponse } from "../utils/response";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+interface UserPayload {
+  id: string;
+  email: string;
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: UserPayload;
+    }
+  }
+}
+
+const requireAuth = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const token = req.header("Authorization")?.replace("Bearer ", "");
+    if (!token) {
+      return res.status(400).json(errorResponse("LOGIN NEEDED!"));
+    }
+
+    const payload = jwt.verify(token, process.env.JWTSECRET!) as UserPayload;
+    req.currentUser = payload;
+
+    next();
+  } catch (err) {
+    return res.status(401).json(errorResponse("Authentication failed"));
+  }
+};
+
+export default requireAuth;
